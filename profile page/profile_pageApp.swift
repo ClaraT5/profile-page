58dec9990758f67d3ee8e2592007e57b8f6b691d
Clara B <claratamsin5@gmail.com>
2023-08-15 14:39:39 -0400

    Initial Commit

---
+++ profile page/profile_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_pageApp.swift
+//  profile page
+//
+//  Created by Clara T Brown on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct profile_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
