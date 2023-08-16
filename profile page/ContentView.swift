333503a25117157bac6de51955f66af4de0482cd
Clara B <claratamsin5@gmail.com>
2023-08-16 15:46:37 -0400

    Centered text

--- profile page/ContentView.swift
+++ profile page/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userUsername = "AAAAAAAAAAAAAAAAAAA"
+    @State private var userPassword = "BBBBBBBBBBBBBBBBBBB"
+    @State private var userFirebase = "CCCCCCCCCCCCCCCCCCC"
+    @State var title = "1"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack {
+                Image("profile-icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                    .clipped()
+                    .padding(.bottom, 200)
+                
+                
+                Text("User Profile").font(.system(size: 20).bold()).foregroundColor(.white)
+                    .padding(.bottom, 200)
+                
+                
+               Spacer()
+            }.background(Color.blue)
+            
+            VStack {
+                Text("Username").font(.system(size: 20).bold()).foregroundColor(.white)
+                    .padding()
+                Text(self.userUsername).font(.system(size: 20)).foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                
+                
+                Text("Password").font(.system(size: 20).bold()).foregroundColor(.white)
+                    .padding()
+                Text(self.userPassword).font(.system(size: 20)).foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("(Optional) Firebase info").font(.system(size: 20).bold()).foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                Text(self.userFirebase).font(.system(size: 20)).foregroundColor(.white).multilineTextAlignment(.center)
+            }
+            Spacer()
+        }.background(Color.blue)
+        
     }
 }
 
@@ -24,3 +62,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
